ecabefd36116885cf006d5b833a968ffb1251b49
wangzuobin.wzb <wangzuobin.wzb@alibaba-inc.com>
2022-01-04 14:43:51 +0800

    Creational Pattern - 3kinds Factory & Builder

---
+++ Java-training-DesignPattern/src/main/java/behavioralpattern/EventListenerPattern.java
@@ -0,0 +1,99 @@
+import java.util.ArrayList;
+import java.util.EventObject;
+import java.util.List;
+
+/**
+ * --- 监听器模式 ---
+ * 1. 包含三个角色：事件监听器，事件对象，事件源
+ * 2. 新建事件源
+ * 3. 给事件源添加监听器【同时在监听器里定义监听器的匿名内部类,完成监听器的 handleEvent(事件)的方法,同时判断事件类型,给出对应处理逻辑】
+ * 4. 定义事件对象,构造函数可以传入 Obj类型
+ * 5. 通过 事件源.notifyListener(Event),来调用监听器的 handleEvent(事件)方法
+ */
+public class EventListenerPattern {
+    public static void main(String[] args) {
+        EventSource eventSource = new EventSource(); // 事件源
+
+        eventSource.addListener(new EventListener() { // 事件源 调用监听器的一个方法，并传递事件对象
+
+            public void handleEvent(MyEventObject eventObject) {
+                eventObject.doEvent();
+
+                if (eventObject.getSource().equals("新增好友申请")) {
+                    System.out.println("新增好友成功"); // 回调
+                } else if (eventObject.getSource().equals("删除好友申请")) {
+                    System.out.println("删除好友成功"); // 回调
+                }else System.out.println("暂时不支持该操作.");
+            }
+        });
+
+        eventSource.addListener(new EventListener() { // 事件源 调用监听器的一个方法，并传递事件对象
+            public void handleEvent(MyEventObject eventObject) {
+                eventObject.doEvent();
+                if (eventObject.getSource().equals("新增好友申请")) {
+                    System.out.println("新增好友成功日志插入"); // 回调
+                } else if (eventObject.getSource().equals("删除好友申请")) {
+                    System.out.println("删除好友成功日志插入"); // 回调
+                }else System.out.println("empty.");
+            }
+        });
+
+        MyEventObject eventObject = new MyEventObject("新增好友申请"); // 事件对象
+        MyEventObject eventObject2 = new MyEventObject("删除好友申请"); // 事件对象
+        MyEventObject eventObject3 = new MyEventObject("隐藏好友申请"); // 事件对象
+
+        eventSource.notifyListenerEvent(eventObject); // 触发事件
+        eventSource.notifyListenerEvent(eventObject2); // 触发事件
+        eventSource.notifyListenerEvent(eventObject3); // 触发事件
+    }
+}
+
+/**
+ * 事件监听器接口,在实际调用时可以通过匿名内部类实现,也可以提前手动实现
+ */
+interface EventListener extends java.util.EventListener {
+    //事件处理
+    void handleEvent(MyEventObject eventObject);
+
+}
+
+/**
+ * 事件对象,作为一个传递值,构造函数接收 Obj类型参数【所以支持各种类型的事件】
+ */
+class MyEventObject extends java.util.EventObject {
+
+    public MyEventObject(Object source) {
+        super(source);
+    }
+
+    public Object getSource() {
+        return source;
+    }
+
+    public void doEvent() {
+        System.out.println("准备操作：\n" + this.getSource());
+    }
+}
+
+/**
+ * 事件源, 带有一个监听器的列表, 同时可以增删监听器, 并且通过 nofityListenerEvent()方法通知监听器去调用它的 handleEvent()方法.
+ */
+class EventSource {
+
+    // 监听器列表，监听器的注册 加入此列表
+    private List<EventListener> listeners = new ArrayList<EventListener>();
+
+    public void addListener(EventListener eventListener) {
+        listeners.add(eventListener);
+    }
+
+    public void removeListener(EventListener eventListener) {
+        listeners.remove(eventListener);
+    }
+
+    public void notifyListenerEvent(MyEventObject eventObject) {
+        for (EventListener eventListener : listeners) {
+            eventListener.handleEvent(eventObject);
+        }
+    }
+}
---
+++ Java-training-DesignPattern/src/main/java/behavioralpattern/ObserverPattern.java
@@ -0,0 +1,147 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * --- 观察者模式 ---
+ * 观察者模式组成结构:
+ *      1.被观察者接口【①添加观察者 ②删除观察者 ③通知观察者】
+ *      2.被观察者实现【实现接口方法，添加删除方法实现通过可变参数可以快速管理】
+ *      3.观察者接口【①定义响应被观察者通知的响应】
+ *      4.观察者实现【初始化+实现具体的响应被观察者通知的响应】
+ */
+public class ObserverPattern {
+    public static void main(String[] args) {
+        //创建观察者
+        IObserver observers1 = new Observers("王大锤");
+        IObserver observers2 = new Observers2("李大锤");
+        IObserver observers3 = new Observers3("乔大锤");
+        IObserver observers4 = new Observers4("余大锤");
+        //创建主题
+        IPrettyGirl angelababy = new Angelababy();
+        //主题添加观察者
+        angelababy.addObservers(observers1,observers2,observers3,observers4);
+        //发送消息通知
+        angelababy.notifyAllObservers("我不开心");
+        //删除观察者
+        System.out.println("----------");
+        angelababy.deleteObservers(observers1,observers2);
+        angelababy.notifyAllObservers("我不开心");
+    }
+}
+
+/*
+ * 抽象主题角色
+ * 类型是接口或者抽象类
+ */
+interface IPrettyGirl {
+    public void addObservers(IObserver... observers);//添加观察者
+
+    public void deleteObservers(IObserver... observers);//删除观察者
+
+    public void notifyAllObservers(String msg);//通知所有的观察者
+}
+
+
+/**
+ * 具体主题
+ */
+class Angelababy implements IPrettyGirl {
+
+    // 观察者集合
+    private List<IObserver> mList = new ArrayList<IObserver>();// 注意这里集合的泛型用的是接口类型
+
+    public void addObservers(IObserver... observers) {
+        for (IObserver iObserver : observers) {
+            mList.add(iObserver);
+        }
+    }
+
+    public void deleteObservers(IObserver... observers) {
+        for (IObserver iObserver : observers) {
+            mList.remove(iObserver);
+        }
+    }
+
+    public void notifyAllObservers(String msg) {
+        System.out.println("angelababy: " + msg);
+        for (IObserver list : mList) {
+            list.UpdateMsg(msg);
+        }
+    }
+}
+
+/**
+ * 抽象观察者
+ */
+interface IObserver {
+    public void UpdateMsg(String msg);
+}
+
+
+/**
+ * 具体观察者1
+ */
+class Observers implements IObserver {
+    private String name;
+
+    public Observers(String name) {
+        this.name = name;
+    }
+
+    public void UpdateMsg(String msg) {
+        if ("我不开心".equals(msg)) {
+            System.out.println(name + "说：我们去打游戏");
+        }
+    }
+}
+/**
+ * 具体观察者
+ */
+class Observers2 implements IObserver {
+    private String name;
+
+    public Observers2(String name) {
+        this.name = name;
+    }
+
+    public void UpdateMsg(String msg) {
+        // TODO 自动生成的方法存根
+        if ("我不开心".equals(msg)) {
+            System.out.println(name + "说：我们去看电影");
+        }
+    }
+}
+/**
+ * 具体观察者3
+ */
+class Observers3 implements IObserver {
+    private String name;
+
+    public Observers3(String name) {
+        this.name = name;
+    }
+
+    public void UpdateMsg(String msg) {
+        // TODO 自动生成的方法存根
+        if ("我不开心".equals(msg)) {
+            System.out.println(name + "说：我们去嘿嘿嘿");
+        }
+    }
+}
+/**
+ * 具体观察者4
+ */
+class Observers4 implements IObserver {
+    private String name;
+
+    public Observers4(String name) {
+        this.name = name;
+    }
+
+    public void UpdateMsg(String msg) {
+        // TODO 自动生成的方法存根
+        if ("我不开心".equals(msg)) {
+            System.out.println(name + "说：关我屁事");
+        }
+    }
+}
